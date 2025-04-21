d793b21ae3a5d787a43ea75e2480684415022ec8
nirmal5601 <nirmal.das@softsuave.org>
2025-04-22 02:33:36 +0530

    serverConfig

--- frontend/src/config/serverApiConfig.js
+++ frontend/src/config/serverApiConfig.js
@@ -26,11 +26,11 @@
 export const API_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE == 'remote'
     ? import.meta.env.VITE_BACKEND_SERVER + 'api/'
-    : 'http://18.224.166.188:8888/api/';
+    : 'http://3.132.133.178:8888/api/';
 export const BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER
-    : 'http://18.224.166.188:8888/';
+    : 'http://3.132.133.178:8888/';
  
 export const WEBSITE_URL = import.meta.env.PROD
   ? 'http://cloud.idurarapp.com/'
@@ -38,7 +38,7 @@ export const WEBSITE_URL = import.meta.env.PROD
 export const DOWNLOAD_BASE_URL =
   import.meta.env.PROD || import.meta.env.VITE_DEV_REMOTE
     ? import.meta.env.VITE_BACKEND_SERVER + 'download/'
-    : 'http://18.224.166.188:8888/download/';
+    : 'http://3.132.133.178:8888/download/';
 export const ACCESS_TOKEN_NAME = 'x-auth-token';
  
 export const FILE_BASE_URL = import.meta.env.VITE_FILE_BASE_URL;
